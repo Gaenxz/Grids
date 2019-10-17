5b090ae50f0dc26f3c82db4ee73bead4a926086c
Loc <locp@magenest.com>
2019-10-17 14:41:44 +0700

    cleancode

--- Plugin/GridJoinCollection.php
+++ Plugin/GridJoinCollection.php
@@ -1,56 +1,37 @@
 <?php
 namespace Magenest\Grids\Plugin;
 
-use Magento\Framework\Message\ManagerInterface as MessageManager;
-use Magento\Sales\Model\ResourceModel\Order\Grid\Collection as SalesOrderGridCollection;
-use Magento\Customer\Model\ResourceModel\Grid\Collection as CustomerGridCollection;
 use Magento\Framework\View\Element\UiComponent\DataProvider\CollectionFactory;
 
 class GridJoinCollection
 {
-    private $messageManager;
-
-    private $salesorderCollection;
-
-    private $customerCollection;
-
-    public function __construct
-    (
-        MessageManager $messageManager,
-        SalesOrderGridCollection $salesorderCollection,
-        CustomerGridCollection $customerCollection
-    ) {
-
-        $this->messageManager = $messageManager;
-        $this->salesorderCollection = $salesorderCollection;
-        $this->customerCollection = $customerCollection;
-    }
-
     public function afterGetReport(
         CollectionFactory $subject,
         $collection,
         $requestName
     ) {
-        $result = $requestName;
-        if ($requestName == 'sales_order_grid_data_source') {
-                $select = $collection->getSelect();
-                $select->joinLeft(
-                    ["secondTable" => $collection->getTable("magenest_custom_column")],
-                    'main_table.increment_id = secondTable.id',
-                    array('custom_column')
-                );
-                return $collection;
-        }
-        elseif($requestName == 'customer_listing_data_source')
+        switch ($requestName)
         {
-            $select = $collection->getSelect();
-            $select->joinLeft(
-                ["secondTable" => $collection->getTable("magenest_custom_column")],
-                'main_table.entity_id = secondTable.id',
-                array('custom_column')
-            );
-            return $collection;
+            case 'sales_order_grid_data_source' :
+                $mainID = 'increment_id';
+                return $collection = $this->joinTable( $collection, $mainID);
+                break;
+            case 'customer_listing_data_source' :
+                $mainID = 'entity_id';
+                return $collection = $this->joinTable( $collection, $mainID);
+                break;
+            default : return $collection;
         }
-        return $result;
+    }
+
+    private function joinTable($collection, $mainID)
+    {
+        $select = $collection->getSelect();
+        $select->joinLeft(
+            ["secondTable" => $collection->getTable("magenest_custom_column")],
+            'main_table.' . $mainID . ' = secondTable.id',
+            array('custom_column')
+        );
+        return $collection;
     }
 }
